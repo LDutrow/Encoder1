4066ef024d49ca6b71bc1107bed223425497eb72
luke <ldutrow@ufl.edu>
2024-10-23 21:26:32 -0400

    k

---
+++ encoder.py
@@ -0,0 +1,43 @@
+def encode_password(password):
+    if len(password) != 8 or not password.isdigit():
+        raise ValueError("Password must be an 8-digit string containing only integers.")
+    encoded_password = ''.join(str((int(digit) + 3) % 10) for digit in password)
+    return encoded_password
+
+def main():
+    stored_password = None
+    encoded_password = None
+
+    while True:
+        print("Menu")
+        print("-------------")
+        print("1. Encode")
+        print("2. Decode")
+        print("3. Quit")
+        
+        option = input("Please enter an option: ")
+
+        if option == '1':
+            password = input("Please enter your password to encode: ")
+            try:
+                encoded_password = encode_password(password)
+                stored_password = password
+                print("Your password has been encoded and stored!")
+            except ValueError as e:
+                print(e)
+
+        elif option == '2':
+            if encoded_password and stored_password:
+                print(f"The encoded password is {encoded_password}, and the original password is {stored_password}.")
+            else:
+                print("No password has been encoded yet.")
+
+        elif option == '3':
+            print("Exiting the program.")
+            break
+
+        else:
+            print("Invalid option, please try again.")
+
+if __name__ == "__main__":
+    main()
